7cf6cd822882a599fac96e196566084af24f7df6
alethevenard <114709512+alethevenard@users.noreply.github.com>
2023-10-21 17:32:04 -0300

    Conf do Bd e da tabela Jogo

---
+++ mf-dev-backend-2023/Migrations/20231021202631_M01-AddTableJogo.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mf_dev_backend_2023.Migrations
+{
+    public partial class M01AddTableJogo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jogos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Loja = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumDeJogadores = table.Column<int>(type: "int", nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    Mecanicas = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Temas = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Valor = table.Column<int>(type: "int", nullable: false),
+                    PeriodoDeLocacao = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jogos");
+        }
+    }
+}
---
+++ mf-dev-backend-2023/Models/Jogo.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2023.Models
+{
+    [Table("Jogos")]
+    public class Jogo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public String Loja { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int NumDeJogadores { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Mecanicas { get; set; }
+
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Temas { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int Valor { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int PeriodoDeLocacao { get; set; }
+    }
+}
